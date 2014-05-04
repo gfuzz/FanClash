b51882d2dca6bae55851ac23461683778ece41ce
Jose R. Borja <chakaitos@gmail.com>
2014-05-04 14:35:35 -0500

    Add profiles for users, add gravatars, and basic profile layout

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe ProfilesController do
 
+	before { @user = User.new(first_name: "Jose", 
+  													last_name: "Borja", 
+  													username: "chakaitos", 
+  													email: "chakaitos@fanclash.com",
+  													password: "password", 
+  													password_confirmation: "password") }
+
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+    it "returns a profile based on username" do
+      get :show, id: @user.username
+      response.status.should eq(200)
+    end
+
+    it "render a 404 on profile not found" do
+      get :show, id: "nothing"
+      response.status.should eq(404)
     end
   end
 

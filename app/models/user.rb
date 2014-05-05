af37f5a369400face4c1104ff5c925c55c3be03b
Jose R. Borja <chakaitos@gmail.com>
2014-05-05 09:35:35 -0500

    Fix bug that was not adding email to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :login, :name, :email
+	attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

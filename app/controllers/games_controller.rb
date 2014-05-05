c7cb153e17b815b00ff314692228615160fd750c
Jose R. Borja <chakaitos@gmail.com>
2014-05-05 18:23:52 -0500

    Add index and show methods for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,13 @@ class GamesController < ApplicationController
   end
 
   def show
+  	@players = DraftedPlayer.where(game_id: params[:id])
+  	@game = Game.find(params[:id])
+  	if @game
+  		render action: :show
+  	else
+	  	render file: 'public/404', status: 404, formats: [:html]
+	  end
   end
 
   def update
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,6 @@
 class ProfilesController < ApplicationController
   def show
   	@user = User.find_by_username(params[:id])
-  	# binding.pry
   	if @user
   		render action: :show
   	else

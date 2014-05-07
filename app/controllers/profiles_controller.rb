97c4a6de6fc4a2b0b15634bb1588c0b0f85211a2
Jose R. Borja <chakaitos@gmail.com>
2014-05-07 17:50:22 -0500

    Refactor app layout to use navbar as a partial, add user games to the user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,15 @@ class ProfilesController < ApplicationController
   def show
   	@user = User.find_by_username(params[:id])
   	if @user
+  		user = ParticipatingUser.where(:user_id => current_user.id)
+		  @game_ids = []
+		  user.each do |e|
+		    @game_ids << e.game_id
+		  end
+		  @games = []
+		  @game_ids.each do |game_id|
+		    @games << Game.find(game_id)
+		  end
   		render action: :show
   	else
 	  	render file: 'public/404', status: 404, formats: [:html]

01d8b064cefe2fb8220732254ffc38f1b4403afb
Drew Garcia <drewgarcia23@gmail.com>
2014-05-07 16:35:23 -0500

    Refactored and commented some of the GamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,33 @@
 class GamesController < ApplicationController
   # Lists all the games
   def index
-    @games = Game.all
+  @games = Game.all
   end
 
+  # Checks if user is signed in, they can buy-in and view a game to submit.
   def show
-  	if user_signed_in?
+    if user_signed_in?
+      @players = DraftedPlayer.where(game_id: params[:id])
+      @game_id = params[:id]
 
-	  	@players = DraftedPlayer.where(game_id: params[:id])
-	    @game_id = params[:id]
+      start_place = 1
+      end_place = 5
 
-	    start_place = 1
-	    end_place = 5
+      while start_place <= end_place do
+        instance_variable_set "@tier#{start_place}",  DraftedPlayer.where(game_id: params[:id], tier: start_place)
+        start_place += 1
+      end
 
-	    while start_place <= end_place do
-	      instance_variable_set "@tier#{start_place}",  DraftedPlayer.where(game_id: params[:id], tier: start_place)
-	      start_place += 1
-	    end
+      @game = Game.find(params[:id])
 
-	  	@game = Game.find(params[:id])
-	  	if @game
-	  		render action: :show
-	  	else
-		  	render file: 'public/404', status: 404, formats: [:html]
-		  end
-		else
-			flash[:alert] = 'You need to sign in or create an account in order to play.'
-			redirect_to '/'
-		end
+      if @game
+        render action: :show
+      else
+        render file: 'public/404', status: 404, formats: [:html]
+      end
+    else
+      flash[:alert] = 'You need to sign in or create an account in order to play.'
+      redirect_to '/'
+    end
   end
 end

73c2437feaa0d4ded4ab2d5eb13744c7c6f4a9d1
Drew Garcia <drewgarcia23@gmail.com>
2014-05-08 10:45:14 -0500

    Changed our show.html.erb file to read out each player instead of calling an each do for each tier. Thus giving us more flexibility with the html later.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,6 +18,26 @@ class GamesController < ApplicationController
         start_place += 1
       end
 
+
+      # The following grabs each player object and sets it to a variable
+      tierplayer1 = @tier1[0]
+      tierplayer2 = @tier1[1]
+      tierplayer3 = @tier2[0]
+      tierplayer4 = @tier2[1]
+      tierplayer5 = @tier3[0]
+      tierplayer6 = @tier3[1]
+      tierplayer7 = @tier4[0]
+      tierplayer8 = @tier4[1]
+      tierplayer9 = @tier5[0]
+      tierplayer10 = @tier5[1]
+
+      # This takes the player object and searches each player in the drafted players
+      # and the players database.
+      for x in (1..10) do
+        eval "\@drafted_player#{x} = DraftedPlayer.find(tierplayer#{x}.id)"
+        eval "\@player#{x} = Player.find(@drafted_player#{x}.player_id)"
+      end
+
       @game = Game.find(params[:id])
 
       if @game

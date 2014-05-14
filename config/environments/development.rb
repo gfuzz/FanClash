7f512525a7b97860e3886dcda762ca5508de19aa
Jose R. Borja <chakaitos@gmail.com>
2014-05-14 17:19:42 -0500

    Switch mailing to Mandrill

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,12 +39,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.mandrillapp.com",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: ENV["MANDRILL_DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_PASSWORD"]
   }
 end

9c0ac00055f9fa6d9fb6598bc03fd4d38c9475e8
Jose R. Borja <chakaitos@gmail.com>
2014-05-19 00:48:15 -0500

    Fix broken migration

--- db/migrate/20140513155817_add_tiebreaker.rb
+++ db/migrate/20140513155817_add_tiebreaker.rb
@@ -1,6 +1,5 @@
 class AddTiebreaker < ActiveRecord::Migration
   def change
-    remove_column :participating_users, :tiebreaker, :integer, :default => 0
     add_column :participating_users, :tiebreaker, :integer, :default => 0
   end
 end
